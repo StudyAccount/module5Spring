fe44258f5fc88d31aac4d750d3daeb0f15392e08
SleepWalker <vityagorbenko@yandex.ru>
2016-11-14 00:26:00 +0200

    initial commit

---
+++ src/main/java/com/schoology/app/calculatorspring/Bootstrap.java
@@ -0,0 +1,13 @@
+package com.schoology.app.calculatorspring;
+
+
+/**
+ * Created by User on 13.11.2016.
+ */
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/schoology/app/calculatorspring/configuration/AppConfig.java
@@ -0,0 +1,13 @@
+package com.schoology.app.calculatorspring.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by User on 13.11.2016.
+ */
+
+@Configuration
+public class AppConfig {
+
+
+}
