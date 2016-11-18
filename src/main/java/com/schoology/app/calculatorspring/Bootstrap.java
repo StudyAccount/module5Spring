7cb62b790c6ee3ec63b0b8e5b0f10578701757de
SleepWalker <vityagorbenko@yandex.ru>
2016-11-18 05:06:02 +0200

    first steps

--- src/main/java/com/schoology/app/calculatorspring/Bootstrap.java
+++ src/main/java/com/schoology/app/calculatorspring/Bootstrap.java
@@ -1,6 +1,11 @@
 package com.schoology.app.calculatorspring;
 
 
+import com.schoology.app.calculatorlibrary.calculator.Calculator;
+import com.schoology.app.calculatorspring.configuration.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Created by User on 13.11.2016.
  */
@@ -8,6 +13,10 @@ public class Bootstrap {
 
     public static void main(String[] args) {
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        System.out.println( calculator.calculate("+,3,4"));
 
     }
+
 }
--- src/main/java/com/schoology/app/calculatorspring/configuration/AppConfig.java
+++ src/main/java/com/schoology/app/calculatorspring/configuration/AppConfig.java
@@ -1,8 +1,11 @@
 package com.schoology.app.calculatorspring.configuration;
 
-import org.springframework.context.ApplicationContext;
+import com.schoology.app.calculatorlibrary.calculator.Calculator;
+import com.schoology.app.calculatorlibrary.calculator.SimpleCalculator;
+import com.schoology.app.calculatorlibrary.registry.Registry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Created by User on 13.11.2016.
@@ -11,7 +14,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @Configuration
 public class AppConfig {
 
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext();
+    @Bean
+    @Scope("prototype")
+    public Calculator calculator(){
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+
+        simpleCalculator.init();
+        return simpleCalculator;
+    }
+
+    @Bean
+    public Registry registry(){
+        return new Registry();
+    }
 
 
 }
